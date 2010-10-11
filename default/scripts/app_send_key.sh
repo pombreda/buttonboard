c9f53420651111be08fc5f87275047c22e2fb706
anville <anville@09dc662d-1d9a-548b-8a8a-a3375011ddee>
2010-10-11 06:12:13 +0000

    Added app_send_key script

---
+++ default/scripts/app_send_key.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Takes up to 3 args:
+# App name
+# key to send
+# optional "using" 
+
+if [ "$3" != "" ]
+then
+	USING="using ($3)"
+else
+	USING=""
+fi
+echo using=$USING
+
+/usr/bin/osascript <<EOF
+
+tell application "$1"
+	activate
+	tell application "System Events" to keystroke $2 $USING
+end tell
+
