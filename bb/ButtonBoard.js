0a65be77f5cc5bd3156429ef9874b4a75d71162d
anville <anville@09dc662d-1d9a-548b-8a8a-a3375011ddee>
2010-10-13 03:04:41 +0000

    Cleaned up, removed dead code

--- bb/ButtonBoard.js
+++ bb/ButtonBoard.js
@@ -30,7 +30,7 @@ function run_cmd(cmd, conf, output)
 				
 		try
 		{
-			if(navigator.appName == "Microsoft Internet Explorer") 
+			if (navigator.appName == "Microsoft Internet Explorer") 
 			{
 				//add a random (more or less) string to prevent the over-agressive caching of IE
 				http_cmd.open("GET", "/cmd/"+cmd+"?foo="+new Date().getTime(), true);
@@ -61,6 +61,20 @@ function run_cmd(cmd, conf, output)
 						{
 							alert(items[1]);
 						}
+						else if (items[0] != "Done")
+						{
+							//something went wrong.  do we need to login again?
+							//See if we got the Login Page
+							if (http_cmd.responseText.indexOf("<title>ButtonBoard Login</title>")!=-1)
+							{	
+								//Got it.  Redirect to show login page
+								window.location = '/login'
+							}
+							else
+							{
+								alert("Something has gone wrong.  Clear your cache and check your password!");
+							}
+						}
 					}
 					
 				}
@@ -68,101 +82,4 @@ function run_cmd(cmd, conf, output)
 		http_cmd.send(null);
 	}
 }
-
-
-
-
-var xmlhttp  = new XMLHttpRequest();
-
-/*
-//----------------------------------------------------------------
-function parse_xml()
-{
-	if (xmlhttp.readyState == 4) 
-	{
-		//alert("Readystate:" + xmlhttp.readyState + "  xmlhttp.status =" + xmlhttp.status );
-		
-		//alert(typeof xmlhttp.responseXML)  ;
-		if (xmlhttp.responseXML == null)
-		{
-			alert("No XML data :-( ");
-		}
-		else
-		{
-			xmlDoc=xmlhttp.responseXML ; 
-			buttonrows=xmlDoc.getElementsByTagName("buttonrow");
-			cmd_list=xmlDoc.getElementsByTagName("cmd");
-			
-			tableHTML = "<table>\n";
-			//walk through the layout, find each command
-			for (row =0 ; row < buttonrows.length; row++)
-			{
-				// go twice for each layout row
-				for(part = 0; part < 2; part++)
-				{
-					tableHTML += "\t<tr>\n";
-					rownode=buttonrows.item(row);
-					for (i = 0; i < rownode.childNodes.length; i++)
-					{
-						thisnode = rownode.childNodes.item(i);
-
-						if (thisnode.tagName == "item")
-						{
-							cmd_name = thisnode.attributes.getNamedItem("n").nodeValue;
-
-							// find the command with this name
-							found = false;
-							var cmd_node;
-
-							for ( c= 0; c < cmd_list.length; c++)
-							{
-								cmd_node = cmd_list.item(c);
-								if (cmd_node.attributes.getNamedItem("name").nodeValue == cmd_name)
-								{
-									found = true;
-
-									break;
-								}
-							}
-
-							var elementHTML = "";
-							if (found)
-							{	
-								if (part == 0)
-								{								
-									elementHTML += "<a href=\"#\"><img src=\"";
-									elementHTML += cmd_node.getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-									elementHTML += "\" onclick=\"run_cmd('" ;
-									elementHTML += cmd_name ;
-									elementHTML += "','" ;
-									elementHTML += cmd_node.getElementsByTagName("confirm")[0].childNodes[0].nodeValue ;
-									elementHTML += "')\" /></a></td>\n" ;	
-								}
-								else
-								{
-									elementHTML += cmd_node.getElementsByTagName("label")[0].childNodes[0].nodeValue;
-								}
-							}
-							
-							tableHTML += "\t\t<td>" + elementHTML + "</td>\n";
-						}
-					}
-					tableHTML += "\t</tr>\n";
-				}
-			}
-			tableHTML += "</table>\n";
-			
-			document.getElementById('button_table').innerHTML = tableHTML;
-		}
-	}
-}
-//----------------------------------------
-*/
-function make_table() 
-{
-	xmlhttp.open("GET", "cmds.xml", true);
-	xmlhttp.onreadystatechange=parse_xml;
-	xmlhttp.send(null);
-}
-
 
